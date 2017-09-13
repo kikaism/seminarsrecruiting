1735195395ed8fdcf1f149361530fce24947fdee
kikaism <kiko.luta0121.fp@gmail.com>
2017-09-13 21:57:11 +0900

    .

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.send('きかだぞお!')
+  })
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
+
